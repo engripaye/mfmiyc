63c6b4a1bdeda72fa63232cc2e4b2946021c153e
mac <@b.tunde.ipaye@gmail.com>
2026-07-13 18:15:56 +0100

    commit init by ENGR IPAYE

---
+++ src/main/java/dev/engripaye/mfmiyc/MfmiycApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.mfmiyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfmiycApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfmiycApplication.class, args);
+    }
+
+}
